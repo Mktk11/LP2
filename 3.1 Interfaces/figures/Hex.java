d0c14f09f4bf0062e5fc5dbf61de46cd2d8518c3
Mktk11 <92392198+Mktk11@users.noreply.github.com>
2021-10-18 10:08:03 -0300

    Create Hex.java

---
+++ 3.1 Interfaces/figures/Hex.java
@@ -0,0 +1,19 @@
+package figures;
+
+import java.awt.*;
+
+public class Hex extends Figure{
+	public Hex(int x,int y,int w,int h,Color preencheCor,Color corBorda){
+		super(x,y,w,h,preencheCor,corBorda);
+	}
+	public void paint (Graphics g,boolean focused) {
+        Graphics2D g2d = (Graphics2D) g;
+        int x1[] = {this.x+w/5,this.x+w*4/5,this.x+w,this.x+w*4/5,this.x+w/5,this.x*(w/w)};
+        int y1[] = {this.y*(h/h),this.y*(h/h),this.y+(this.h/2),this.y+h,this.y+h,this.y+(this.h)/2};
+        g2d.setColor(this.preencheCor);
+        g2d.fillPolygon(x1, y1, 6); 
+        g2d.setColor(this.corBorda);
+        g2d.drawPolygon(x1, y1, 6);
+    }
+
+}
