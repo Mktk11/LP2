1f137df2d105ca29bab69d418640c6f64b98f8a0
Mktk11 <92392198+Mktk11@users.noreply.github.com>
2021-10-25 21:10:30 -0300

    Update ListApp.java

--- Projeto 1/ListApp.java
+++ Projeto 1/ListApp.java
@@ -2,11 +2,15 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.Color;
 import java.awt.event.MouseEvent;
+
 import javax.swing.*;
 import javax.swing.JFrame;
-import java.util.ArrayList;
 import javax.swing.JPanel;
-import java.awt.Toolkit;
+import java.util.ArrayList;
+
+import java.io.*;
+
+import buttons.Button;
 import figures.*;
 
 class ListApp {
@@ -18,7 +22,9 @@ class ListApp {
 }
 
 class ListFrame extends JFrame {
-    ArrayList<Figure> figs = new ArrayList<Figure>();
+	ArrayList<Figure> figs = new ArrayList<Figure>();
+    ArrayList<Button> buts = new ArrayList<Button>();
+    Button focus_but=null;
     Figure focus = null;
     Point pMouse = null;
 
@@ -36,6 +42,18 @@ class ListFrame extends JFrame {
                 }
             }
         );
+        buts.add(new Button(0,new Rect(20,50,10,10,Color.BLACK,Color.BLACK)));
+        buts.add(new Button(1,new Ellipse(40,50,10,10,Color.BLACK,Color.BLACK)));
+        buts.add(new Button(2,new Triangle(20,70,10,10,Color.BLACK,Color.BLACK)));
+        buts.add(new Button(3,new Hex(40,70,10,10,Color.BLACK,Color.BLACK)));
+        buts.add(new Button(4,new Rect(20,90,10,10,Color.YELLOW,Color.YELLOW)));
+        buts.add(new Button(5,new Rect(40,90,10,10,Color.BLUE,Color.BLUE)));
+        buts.add(new Button(6,new Rect(20,110,10,10,Color.RED,Color.RED)));
+        buts.add(new Button(7,new Rect(40,110,10,10,Color.GREEN,Color.GREEN)));
+        buts.add(new Button(8,new Rect(20,130,10,10,Color.PINK,Color.PINK)));
+        buts.add(new Button(9,new Rect(40,130,10,10,Color.GRAY,Color.GRAY)));
+        buts.add(new Button(10,new Rect(20,150,10,10,Color.ORANGE,Color.ORANGE)));
+        buts.add(new Button(11,new Rect(40,150,10,10,Color.BLACK,Color.BLACK)));
         this.addMouseListener(
             new MouseAdapter(){
                 public void mousePressed(MouseEvent evt){
@@ -44,19 +62,131 @@ class ListFrame extends JFrame {
                         int y = evt.getY();
                         pMouse = getMousePosition();
                         focus = null;
-                        for (Figure fig: figs){
-                            if (fig.clicked(pMouse.x,pMouse.y)){
-                                focus = fig;
-                                dx = focus.x - pMouse.x;
-                                dy = focus.y - pMouse.y;
+                        if ((15 <= pMouse.x && pMouse.x <= 60) && (35 <= pMouse.y && pMouse.y <= 215)) {
+                            focus=null;
+                            for(Button but:buts){
+                                if(but.clicked(pMouse.x,pMouse.y)){
+                                    focus_but=but;
+                                }
+                                repaint();
                             }
                         }
-                        if (focus!=null){
-                            figs.remove(focus);
-                            figs.add(focus);
+                        else if(focus_but!=null){
+                            int i = focus_but.i;
+                            pMouse=getMousePosition();
+                            int w = 50;
+                            int h = 50;
+                            Color corBorda = cores1[contcontorno];
+                            Color preencheCor = cores1[contpreenchimento];
+                            if (i==0){
+                                figs.add(new Rect(x,y, w,h,preencheCor,corBorda));
+                            }
+                            else if(i==1){
+                                figs.add(new Ellipse(x,y,w,h,preencheCor,corBorda));
+                            }
+                            else if(i==2){
+                                figs.add(new Triangle(x,y,w,h,preencheCor,corBorda));
+                            }
+                            else if(i==3){
+                                figs.add(new Hex(x,y,w,h,preencheCor,corBorda));
+                            }
+                            else if(i==4){
+                                for (Figure fig: figs){
+                                    if (fig.clicked(pMouse.x,pMouse.y)){
+                                        focus=fig;
+                                        focus.preencheCor = Color.YELLOW;
+                                        focus.corBorda = Color.YELLOW;
+
+                                    }
+                                }
+                            }
+                            else if(i==5){
+                                for (Figure fig: figs){
+                                    if (fig.clicked(pMouse.x,pMouse.y)){
+                                        focus=fig;
+                                        focus.preencheCor = Color.BLUE;
+                                        focus.corBorda = Color.BLUE;
+                                    }
+                                }
+                            }
+                            else if(i==6){
+                                for (Figure fig: figs){
+                                    if (fig.clicked(pMouse.x,pMouse.y)){
+                                        focus=fig;
+                                        focus.preencheCor = Color.RED;
+                                        focus.corBorda = Color.RED;
+
+                                    }
+                                }
+                            }
+                            else if(i==7){
+                                for (Figure fig: figs){
+                                    if (fig.clicked(pMouse.x,pMouse.y)){
+                                        focus=fig;
+                                        focus.preencheCor = Color.GREEN;
+                                        focus.corBorda = Color.GREEN;
+
+                                    }
+                                }
+                            }
+                            else if(i==8){
+                                for (Figure fig: figs){
+                                    if (fig.clicked(pMouse.x,pMouse.y)){
+                                        focus=fig;
+                                        focus.preencheCor = Color.PINK;
+                                        focus.corBorda = Color.PINK;
+
+                                    }
+                                }
+                            }
+                            else if(i==9){
+                                for (Figure fig: figs){
+                                    if (fig.clicked(pMouse.x,pMouse.y)){
+                                        focus=fig;
+                                        focus.preencheCor = Color.GRAY;
+                                        focus.corBorda = Color.GRAY;
+
+                                    }
+                                }
+                            }
+                            else if(i==10){
+                                for (Figure fig: figs){
+                                    if (fig.clicked(pMouse.x,pMouse.y)){
+                                        focus=fig;
+                                        focus.preencheCor = Color.ORANGE;
+                                        focus.corBorda = Color.ORANGE;
+
+                                    }
+                                }
+                            }
+                            else if(i==11){
+                                for (Figure fig: figs){
+                                    if (fig.clicked(pMouse.x,pMouse.y)){
+                                        focus=fig;
+                                        focus.preencheCor = Color.BLACK;
+                                        focus.corBorda = Color.BLACK;
+
+                                    }
+                                }
+                            }
+                            focus_but=null;
+                            repaint();
                         }
-                        repaint();
-                        
+                        else{
+                            for (Figure fig: figs){
+                                if (fig.clicked(pMouse.x,pMouse.y)){
+                                    focus = fig;
+                                    dx = focus.x - pMouse.x;
+                                    dy = focus.y - pMouse.y;
+                                }
+                            }
+                            if (focus!=null){
+                                figs.remove(focus);
+                                figs.add(focus);
+                            }
+                            repaint();
+                        }
+                            
                     }catch(Exception x){}
                 }
             }
@@ -200,10 +330,13 @@ class ListFrame extends JFrame {
         super.paint(g);
         for(Figure fig: this.figs){
             if(focus==fig){
-                focus.preencherFig(g);
-                focus.desenharBorda(g);
+                focus.preencherFig(g,fig==focus);
+                focus.desenharBorda(g,fig==focus);
             }
-            fig.paint(g);
+            fig.paint(g,fig==focus);
+        }
+        for(Button but: buts){
+            but.paint(g,but==focus_but);
         }
         
         
