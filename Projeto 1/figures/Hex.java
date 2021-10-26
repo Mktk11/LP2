25d747672434e05d42135a7592a7fddb31f7f18c
Mktk11 <92392198+Mktk11@users.noreply.github.com>
2021-10-25 21:06:00 -0300

    Update Hex.java

--- Projeto 1/figures/Hex.java
+++ Projeto 1/figures/Hex.java
@@ -6,7 +6,7 @@ public class Hex extends Figure{
 	public Hex(int x,int y,int w,int h,Color preencheCor,Color corBorda){
 		super(x,y,w,h,preencheCor,corBorda);
 	}
-	public void paint (Graphics g) {
+	public void paint (Graphics g, boolean b) {
         Graphics2D g2d = (Graphics2D) g;
         int x1[] = {this.x+w/5,this.x+w*4/5,this.x+w,this.x+w*4/5,this.x+w/5,this.x*(w/w)};
         int y1[] = {this.y*(h/h),this.y*(h/h),this.y+(this.h/2),this.y+h,this.y+h,this.y+(this.h)/2};
